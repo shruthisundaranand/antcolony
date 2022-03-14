5ba195777854c9040b62536f116f8961b3f2dfc4
nrgopal <78436756+nrgopal@users.noreply.github.com>
2022-03-14 16:42:27 +0100

    Update demo_with_mflops.py

--- demo_with_mflops.py
+++ demo_with_mflops.py
@@ -1,48 +1,16 @@
-import sys, string, os
 import subprocess
 from pyswarm import pso
 import pants
 import math
 import random
 
-def iso3dfd(threads, cache__block_d1):
-# size of the problem in three dimensions
-    n1 = "256"
-    n2 = "256"
-    n3 = "256"
-
-    # number of threads
-    #num_threads = "32"
-    num_threads = string(threads)
-    
-    # number of iterations
-    nreps = "100"
-
-    # cache blocking for each dimension
-    #n1_thrd_block = "256" #str(parms[1])
-    n1_thrd_block = string(cache_block_d1)
-    n2_thrd_block = "12" #str(parms[2])
-    n3_thrd_block = "30" #str(parms[3])
-
-    throughput = os.popen("bin/iso3dfd_dev13_cpu_avx.exe " + n1 + " " + n2 + " " + n3 + " " + num_threads + " " + nreps + " " + n1_thrd_block + " " + n2_thrd_block + " " + n3_thrd_block)
-
-    # Output = os.system("bin/iso3dfd_dev13_cpu_avx.exe " + n1 + " " + n2 + " " + n3 + " " + num_threads + " " + nreps + " " + n1_thrd_block + " " + n2_thrd_block + " " + n3_thrd_block  + " | grep 'throughput' | tr -s ' ' | cut -f2 -d ' '")
-
-    return throughput
-   
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
 def getMflops(output):
-    # replace the above ls -l with the actual exe
     result = ""
    
     try:
         lines = output.read().split("\n")
         for line in lines:
-            if line.__contains__("throughput"):  # change the "iso-3dfd_main.cc" to throughput
+            if line.__contains__("throughput"):  
                 result = line
     finally:
         output.close();
@@ -54,8 +22,29 @@ def getMflops(output):
 
     return flops
 
-# Press the green button in the gutter to run the script.
+def iso3dfd(threads, cache_block_d1):
+# size of the problem in three dimensions
+    n1 = "256"
+    n2 = "256"
+    n3 = "256"
+
+    # number of threads
+    num_threads = str(threads)
+    
+    # number of iterations
+    nreps = "100"
+
+    # cache blocking for each dimension
+    n1_thrd_block = str(cache_block_d1)
+    n2_thrd_block = "12"
+    n3_thrd_block = "30"
+
+    output = os.popen("bin/iso3dfd_dev13_cpu_avx.exe " + n1 + " " + n2 + " " + n3 + " " + num_threads + " " + nreps + " " + n1_thrd_block + " " + n2_thrd_block + " " + n3_thrd_block)
+
+    throughput = getMflops(output)
+    
+    return throughput
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    output = os.popen("bin/iso3dfd_dev13_cpu_avx.exe")
-    print(getMflops(output))
+    print(iso3dfd(32,256))
